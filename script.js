8b2f6e90899418fb002aec5f785cfe4588a0e26a
mahe242 <mahe.cc09@gmail.com>
2025-11-04 15:52:07 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
  console.log("Laboratoire prêt !");
+const nom = "chevaleau calandre";
+let age = 16
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur ="12345"; // motdepasseutilisateur
+  if (motdepasseattendu===motdepasseutilisateur) {
 
+    console.log("Accès autorisé. Bienvenue !");
+} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
